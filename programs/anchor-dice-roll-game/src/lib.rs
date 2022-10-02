5836b1fc2be03a31afc4e32f86f8daad90cb6afe
cihancanayyildiz <cihancanayyildiz@gmail.com>
2022-10-02 22:40:21 +0300

    Init and play functions developed. Unit tests completed.

--- programs/anchor-dice-roll-game/src/lib.rs
+++ programs/anchor-dice-roll-game/src/lib.rs
@@ -5,11 +5,94 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod anchor_dice_roll_game {
     use super::*;
+    use anchor_lang::solana_program::{ program::invoke, system_instruction::transfer };
+    pub fn setup(ctx: Context<Setup>, player: Pubkey, bet_amount: u64) -> Result<()> {
+        let dice_roll = &mut ctx.accounts.dice_roll;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        dice_roll.players = [ctx.accounts.user.key(), player];
+        dice_roll.bump = *ctx.bumps.get("dice_roll").unwrap();
+        dice_roll.bet_amount = bet_amount;
+        dice_roll.game_state = String::from("");
+
+        Ok(())
+    }
+
+    pub fn play(ctx: Context<Play>, player_choices: String) -> Result<()> {
+        let dice_roll = &mut ctx.accounts.dice_roll;
+
+        invoke(
+            &transfer(
+                ctx.accounts.user.to_account_info().key,
+                dice_roll.to_account_info().key,
+                dice_roll.bet_amount
+            ),
+            &[
+                ctx.accounts.user.to_account_info(),
+                dice_roll.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            ]
+        )?;
+
+        let mut num_vec: Vec<i32> = Vec::new();
+
+        for item in player_choices.chars() {
+            if item != ',' {
+                num_vec.push((item as i32) - 0x30);
+            }
+        }
+        let clock: Clock = Clock::get().unwrap();
+        let dice_result: i32 = ((clock.unix_timestamp % 6) + 1).try_into().unwrap();
+
+        // win
+        if num_vec.contains(&dice_result) {
+            dice_roll.game_state = format!("You won! Dice result: {}", dice_result);
+            **dice_roll.to_account_info().try_borrow_mut_lamports()? -= dice_roll.bet_amount;
+            **ctx.accounts.user.try_borrow_mut_lamports()? += 2 * dice_roll.bet_amount;
+        } else {
+            dice_roll.game_state = format!("You lose! Dice result: {}", dice_result);
+        }
+
         Ok(())
     }
 }
+#[derive(Accounts)]
+pub struct Setup<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = DiceRoll::LEN,
+        seeds = ["dice_roll".as_bytes(), user.key().as_ref()],
+        bump
+    )]
+    pub dice_roll: Account<'info, DiceRoll>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    //pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
+}
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Play<'info> {
+    #[account(
+        mut, 
+        seeds = ["dice_roll".as_bytes(), user.key().as_ref()],
+        bump = dice_roll.bump
+    )]
+    pub dice_roll: Account<'info, DiceRoll>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(Default)]
+pub struct DiceRoll {
+    players: [Pubkey; 2],
+    bet_amount: u64,
+    game_state: String,
+    bump: u8,
+}
+
+impl DiceRoll {
+    const LEN: usize = 200;
+}
--- tests/anchor-dice-roll-game.ts
+++ tests/anchor-dice-roll-game.ts
@@ -3,14 +3,53 @@ import { Program } from "@project-serum/anchor";
 import { AnchorDiceRollGame } from "../target/types/anchor_dice_roll_game";
 
 describe("anchor-dice-roll-game", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+    // Configure the client to use the local cluster.
+    let provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
 
-  const program = anchor.workspace.AnchorDiceRollGame as Program<AnchorDiceRollGame>;
+    const program = anchor.workspace
+        .AnchorDiceRollGame as Program<AnchorDiceRollGame>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+    const player = anchor.web3.Keypair.generate();
+
+    it("Is initialized!", async () => {
+        let sig = await provider.connection.requestAirdrop(
+            player.publicKey,
+            1 * anchor.web3.LAMPORTS_PER_SOL
+        );
+        await provider.connection.confirmTransaction(sig);
+
+        const balance = await provider.connection.getBalance(player.publicKey);
+
+        console.log(player.publicKey.toBase58());
+        console.log(balance / anchor.web3.LAMPORTS_PER_SOL);
+
+        // Add your test here.
+        const tx = await program.methods
+            .setup(
+                player.publicKey,
+                new anchor.BN(0.1 * anchor.web3.LAMPORTS_PER_SOL)
+            )
+            .rpc()
+            .catch(console.error);
+        console.log("Your transaction signature", tx);
+    });
+
+    it("Playing the dice game.", async () => {
+        // A dice contains 6 numbers (1,2,3,4,5,6). Player needs to send 3 number in a format like below as string. Player has %50 chance to win.
+
+        // If player wins player gets 2x bet. If player lose it lose bet balance.
+        let ix = program.methods.play("1,2,5");
+
+        const tx = await ix.rpc().catch(console.error);
+        console.log("transaction: ", tx);
+
+        const userdiceroll = (await ix.pubkeys()).diceRoll;
+
+        let data = await program.account.diceRoll.fetch(userdiceroll);
+
+        const balance = await provider.connection.getBalance(player.publicKey);
+        console.log(balance / anchor.web3.LAMPORTS_PER_SOL);
+        console.log(data);
+    });
 });
